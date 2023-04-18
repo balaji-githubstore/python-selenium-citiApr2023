98473c3f0e41cb0bdd7b650592e12490cb84a54e
Balaji <balajidinakaran1@gmail.com>
2023-04-18 13:10:57 +0530

    Complete Mouse actions in demo8_actions.py

---
+++ selenium_concepts/demo8_actions.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(20)
+
+driver.get("https://nasscom.in/")
+
+actions = webdriver.ActionChains(driver)
+actions.move_to_element(driver.find_element(By.XPATH, "//a[text()='Membership']")).perform()
+
+# mouse hover on Become a member
+
+driver.find_element(By.XPATH, "//a[text()='Membership Benefits']").click()
